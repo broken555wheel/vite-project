6bac7f91e6df5db7cafb5e8e92d0d9af1fbaaa3f
David Mwalimu Nzambuli <nzambulidavid@gmail.com>
2023-06-12 10:45:07 +0300

    updates

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,52 @@
-<script setup>
-import { ref } from 'vue'
+<template>
+  <div>
 
-defineProps({
-  msg: String,
-})
+    <h1>{{ msg }}</h1>
 
-const count = ref(0)
+<div class="card">
+  <button type="button" @click="count++">count is {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test HMR
+  </p>
+</div>
+
+<p>
+  Check out
+  <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+    >create-vue</a
+  >, the official Vue + Vite starter
+</p>
+<p>
+  Install
+  <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+  in your IDE for a better DX
+</p>
+<p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  </div>
+</template>
+
+
+<script >
+export default{
+  //this here is the props option
+  props:['msg'],
+  //dat aoption
+  data(){
+    return{
+      count:0,
+    }
+  },
+}
+
+// defineProps({
+//   msg: String,
+// })
+
+// const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</template>
 
 <style scoped>
 .read-the-docs {
