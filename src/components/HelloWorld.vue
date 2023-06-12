4811080fc4d7eaf66e96cf4da4fe08aa9abe0f5e
David Mwalimu Nzambuli <nzambulidavid@gmail.com>
2023-06-12 11:33:20 +0300

    updates

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,43 @@
 <template>
   <div>
-
-    <h1>{{ msg }}</h1>
-
-<div class="card">
-  <button type="button" @click="count++">count is {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test HMR
-  </p>
-</div>
-
-<p>
-  Check out
-  <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-    >create-vue</a
-  >, the official Vue + Vite starter
-</p>
-<p>
-  Install
-  <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-  in your IDE for a better DX
-</p>
-<p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <h1>{{ count }}</h1>
+    <h1>name: {{ name }}</h1>
+    <h1>session: {{ session }}</h1>
+    <h1>date: {{ date }}</h1>
   </div>
 </template>
 
-
-<script >
-export default{
+<script>
+export default {
   //this here is the props option
-  props:['msg'],
+  props: ["msg"],
   //dat aoption
-  data(){
-    return{
-      count:0,
-    }
+  data() {
+    return {
+      count: 0,
+      name: "Ericson Lab",
+      date: "12th June 2023",
+      session: 10,
+    };
   },
-}
 
-// defineProps({
-//   msg: String,
-// })
-
-// const count = ref(0)
+  //methods option
+  methods: {
+    //this method focuses on increasing the nth value of count
+      increaseCountValue(){
+      //get count variable from data option
+      //increment it by one
+      this.count++;
+    },
+    //this method focuses on decreasing the nth value of count
+    decreaseCountValue(){
+      //get count variable from data option
+      //decrement it by one
+      this.count--;
+    },
+  },
+};
 </script>
-
-
 
 <style scoped>
 .read-the-docs {
